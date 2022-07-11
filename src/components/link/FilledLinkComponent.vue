2d2ed4d1b159287d5e25366fc6ae71287c121856
MatveyLh <liakh.matvei@mail.ru>
2022-07-11 10:56:54 +0300

    update styles for links

--- src/components/link/FilledLinkComponent.vue
+++ src/components/link/FilledLinkComponent.vue
@@ -8,25 +8,10 @@ defineProps<{
 </script>
 
 <template>
-  <RouterLink class="link" :to="href">{{ label }}</RouterLink>
+  <RouterLink class="filledLinkStyle" :to="href">{{ label }}</RouterLink>
 </template>
 
 <style scoped lang="scss">
-@import '@/assets/colors.scss';
-
-.link {
-  display: block;
-  font-size: 16px;
-  line-height: 20px;
-  text-decoration: none;
-  transition: background-color 0.2s ease;
-  color: $white;
-  background-color: $primary;
-  padding: 12px 20px;
-  border-radius: 6px;
-
-  &:hover {
-    background-color: $primary__dark;
-  }
-}
+  @import '@/assets/colors.scss';
+  @import './coreStyle.scss';
 </style>
--- src/components/link/LinkComponent.vue
+++ src/components/link/LinkComponent.vue
@@ -8,23 +8,10 @@ defineProps<{
 </script>
 
 <template>
-  <RouterLink class="link" :to="href">{{ label }}</RouterLink>
+  <RouterLink class="defaultLinkStyle" :to="href">{{ label }}</RouterLink>
 </template>
 
 <style scoped lang="scss">
   @import '@/assets/colors.scss';
-
-  .link {
-    display: block;
-    color: $black;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 20px;
-    transition: color 0.2s ease;
-    text-decoration: none;
-
-    &:hover {
-      color: $primary;
-    }
-  }
+  @import './coreStyle.scss';
 </style>
